777053adcae4060ff95572b477e48dfbc872973a
Kosuke-Kg <kosuke.kg1127@gmail.com>
2023-04-27 22:51:01 +0900

    main article 作成

--- src/components/Layouts/Layouts.tsx
+++ src/components/Layouts/Layouts.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../footer/Footer'
 import Header from '../header/Header'
+import styles from './Layouts.module.css'
 
 interface Props {
   children: JSX.Element
@@ -10,7 +11,11 @@ const Layouts = ({ children }: Props): JSX.Element => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={styles.wrapperMain}>
+        <article className={styles.leftArticle}></article>
+        <article className={styles.mainArticle}>{children}</article>
+        <article className={styles.rightArticle}></article>
+      </main>
       <Footer />
     </>
   )
